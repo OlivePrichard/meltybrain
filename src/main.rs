aba514846e42aae31e398833ea7605cb84487c10
Ben Caunt <ben@warmhub.com>
2024-11-14 21:24:33 -0500

    working motor spinup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@
 use embassy_executor::Spawner;
 use esp_backtrace as _;
 use esp_hal::{
-    gpio::Io, i2c::I2c, prelude::*, timer::timg::TimerGroup
+    gpio::Io, i2c::I2c, ledc::{channel, timer, LSGlobalClkSource, Ledc, LowSpeed}, prelude::*, timer::timg::TimerGroup
+
 };
 use esp_println::println;
 
@@ -14,21 +15,54 @@ use esp_println::println;
 #[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
     let peripherals = esp_hal::init(esp_hal::Config::default());
-
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_hal_embassy::init(timg0.timer0);
-
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    let mut i2c0 = I2c::new_async(peripherals.I2C0, io.pins.gpio6, io.pins.gpio7, 400.kHz());
+    let led = io.pins.gpio21; // io.pins.gpio9
 
-    let mut buffer = [0u8; 6];
-    i2c0.write(0x53, &[0x2d, 0x08]).await.unwrap();
-    loop {
-        buffer = [0u8; 6];
-        match i2c0.write_read(0x53, &[0x36], &mut buffer).await {
-            Ok(_) => println!("{:02X?}", &buffer[..6]),
-            Err(e) => println!("{:?}", e),
-        }
+    let mut ledc = Ledc::new(peripherals.LEDC);
+
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+
+    let mut lstimer0 = ledc.get_timer::<LowSpeed>(timer::Number::Timer0);
+
+    lstimer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty5Bit,
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: 8.kHz(),
+        })
+        .unwrap();
+
+    let mut channel0 = ledc.get_channel(channel::Number::Channel0, led);
+    channel0
+        .configure(channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
+        .unwrap();
+
+    // channel0.start_duty_fade(0, 100, 2000).expect_err(
+    //     "Fading from 0% to 100%, at 24kHz and 5-bit resolution, over 2 seconds, should fail",
+    // );
+    channel0.start_duty_fade(0, 50, 500).unwrap();
+    while channel0.is_duty_fade_running() {
     }
+    channel0.start_duty_fade(50, 0, 500).unwrap();
+    while channel0.is_duty_fade_running() {
+    }
+    channel0.start_duty_fade(0, 50, 500).unwrap();
+    while channel0.is_duty_fade_running() {
+    }
+    channel0.start_duty_fade(50, 0, 1500).unwrap();
+    while channel0.is_duty_fade_running() {
+    }
+    channel0.start_duty_fade(0, 70, 1000).unwrap();
+    while channel0.is_duty_fade_running() {
+    }
+    loop {
+        channel0.set_duty(70).unwrap();
+    }
+ 
+
 }
